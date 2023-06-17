27be2c945c22b28814d07079a7e620aa5863a5a6
Ghost <danyfox1.dm@gmail.com>
2023-06-17 18:17:08 -0300

    next_node = nil

--- lib/node.rb
+++ lib/node.rb
@@ -2,9 +2,9 @@ class Node # Contain value_method and the link to the next_node, set bothas nil 
   
   attr_accessor :value, :next_node
 
-  def initialize(value = nil) 
+  def initialize(value=nil, next_node=nil) 
     @value = value
-    @next_node = nil
+    @next_node = next_node
   end
 
 end
