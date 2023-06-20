680eee32063987a2394146991b0fc1c934a4fc03
Ghost <danyfox1.dm@gmail.com>
2023-06-19 21:16:52 -0300

    show process

--- lib/main.rb
+++ lib/main.rb
@@ -3,36 +3,27 @@ require './linked_list.rb'
 
 list = LinkedList.new()
 
-# first = list.append('Primer_valor')
-primero = list.append('Primer_valor')
-segundo = list.append('Segudo_valor')
-tercero = list.append('tercer_valor')
-al_comienzo = list.prepend_('al_principio')
-antes = list.prepend_('antes')
-cuarto = list.append('last_value')
+list.append('two')
+list.append('three')
+list.prepend_('one')
+list.append('four')
+list.append('five')
 
-# p primero.value
-# p segundo.value 
-# p tercero.value 
-# p al_comienzo.value
-# p antes.value
-
-# puts "\nnext_node..."
-# #p primero.next_node
-# p antes.next_node
-# #p tercero.next_node
-
-# puts "\nlist..."
-# puts list.class  
-
-# puts "\nsize..."
-# p list.size 
-
-# puts "\nhead"
-# p list.head
-
-# puts "\ntail..."
-# p list.tail
-
-puts "\nat_index..."
-p list.at(0)
+print "\nAll nodes => "
+list.to_s 
+puts "head => #{list.head_value}"
+puts "size => #{list.size}"
+puts "tail => #{list.tail}"
+puts "at_index(1) => #{list.at(1)}"
+print "remove last => #{list.pop}"
+list.to_s 
+puts "contains(four) => #{list.contains?('four')}"
+puts "contains(five) => #{list.contains?('five')}"
+puts "find(one) return index => #{list.find('one')}"
+puts "find(five) return index => #{list.find('five') || 'nil'}"
+list.insert_at('value', 2)
+print "Insert_at(value, 2) => "
+list.to_s 
+list.remove_at(2)
+print "remove_at(2) => "
+list.to_s
