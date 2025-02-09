6f342d984660de3ffc44c25650df94ea8cc9eb5c
Ridanm <danyfox1.dm@gmail.com>
2025-02-09 19:10:32 -0300

    Code correction with rubocop

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,164 +1,177 @@
-class LinkedList # Represent full list
+# frozen_string_literal: true
 
-  attr_accessor :head 
+# Class linked list
+class LinkedList
+  attr_accessor :head
 
   def initialize
-    @head = nil 
+    @head = nil
   end
 
-  def app_end(value) # adds a new containing value to the end of the list
+  def app_end(value)
     if head.nil?
-      @head = Node.new(value) 
-    else 
-      current = @head 
+      @head = Node.new(value)
+    else
+      current = @head
       current = current.next_node while current.next_node
       current.next_node = Node.new(value)
     end
   end
 
-  def prepend!(value) # adds a new node containing value to the start of the list
+  def prepend!(value)
     new_node = Node.new(value)
-    new_node.next_node = @head 
-    @head = new_node 
+    new_node.next_node = @head
+    @head = new_node
   end
 
-  def size # returns the total number of nodes in the list
+  def size
     count = 0
-    current = @head 
-    while current  
+    current = @head
+    while current
       count += 1
-      current = current.next_node 
+      current = current.next_node
     end
-    count 
+    count
   end
 
-  def size_recursive(node=@head)
-    return 0 if node.nil? 
-    return 1 + size_recursive(node.next_node)
+  def size_recursive(node = @head)
+    return 0 if node.nil?
+
+    1 + size_recursive(node.next_node)
   end
 
-  def head_node # returns the first node in the list 
-    return 'Empty list...' if head.nil? 
+  def head_node
+    return 'Empty list...' if head.nil?
+
     @head
   end
 
-  def tail # return the last node in the list
-    node = @head 
-    ref_last = nil 
+  # return the last node in the list
+  def tail
+    node = @head
+    ref_last = nil
 
-    while node 
+    while node
       ref_last = node
       node = node.next_node
     end
 
-    ref_last  
+    ref_last
   end
 
-  def at(index) # returns the node at the given index
-    index = index.to_i 
-    return 'Index out of range...' if index > size - 1 || index < 0 
+  def at(index)
+    index = index.to_i
+    return 'Index out of range...' if index > size - 1 || index.negative?
 
-    current = @head 
-    count = 0 
-    while current 
-      return current if count == index 
-      current = current.next_node 
-      count += 1 
+    current = @head
+    count = 0
+    while current
+      return current if count == index
+
+      current = current.next_node
+      count += 1
     end
-  end 
+  end
 
-  def pop # removes the last elemnet from the list 
+  def pop
     return if head.nil?
-    return @head = nil if head.next_node.nil? 
-    current = @head 
-    previous = nil 
+    return @head = nil if head.next_node.nil?
 
-    while current.next_node 
-      previous = current 
-      current = current.next_node 
+    current = @head
+    previous = nil
+
+    while current.next_node
+      previous = current
+      current = current.next_node
     end
 
-    previous.next_node = nil 
+    previous.next_node = nil
+    previous
   end
 
-  def contains?(value) # returns true if the passed in value is the list and otherwise returns false
-    return false if head.nil? 
-    current = @head 
+  def contains?(value)
+    return false if head.nil?
 
-    while current 
-      return true if current.data == value 
-      current = current.next_node 
+    current = @head
+
+    while current
+      return true if current.data == value
+
+      current = current.next_node
     end
 
-    false 
+    false
   end
 
-  def contains_recursive?(value, current=@head)
-    return false if current.nil? 
-    return true if current.data == value  
-    return false if current.next_node.nil? 
+  def contains_recursive?(value, current = @head)
+    return false if current.nil?
+    return true if current.data == value
+    return false if current.next_node.nil?
+
     contains_recursive?(value, current.next_node)
   end
 
-
-  def find(value, node = @head, counter = 0) # return the index of the node containing value, or nil if not 
+  def find(value, node = @head, counter = 0)
     return if node.nil?
-    return counter if node.data == value 
-    find(value, node.next_node, counter += 1)
+    return counter if node.data == value
+
+    find(value, node.next_node, counter + 1)
   end
 
-  def to_s(list = @head) # represent linked_list object as string, Must show: ( value ) -> ( value ) -> ( value ) -> nil
-    return puts 'nil' if list.nil? 
+  # represent linked_list object as string, Must show: ( value ) -> ( value ) -> ( value ) -> nil
+  def to_s(list = @head)
+    return puts 'nil' if list.nil?
+
     print "( #{list.data} ) -> "
     to_s(list.next_node)
   end
- 
-  # Extra credit 
+
+  # Extra credit
   def insert_at(value, index)
     index = index.to_i
-    return 'Wrong index...' if index < 0 || index > size - 1
-    current = @head 
+    return 'Wrong index...' if index.negative? || index > size - 1
+
+    current = @head
     insert = Node.new(value)
-    prev = nil 
+    prev = nil
     count = 0
 
-    while count < index 
-      prev = current 
-      count += 1 
-      current = current.next_node 
+    while count < index
+      prev = current
+      count += 1
+      current = current.next_node
     end
 
-    if index == 0 
-      insert.next_node = current 
-      @head = insert 
-      return head
-    else 
-      insert.next_node = current
-      prev.next_node = insert 
-      return head 
+    insert.next_node = current
+    if index.zero?
+      @head = insert
+    else
+      prev.next_node = insert
     end
+    head
   end
 
   def remove_at(index)
-    index = index.to_i 
-    return 'Wrong index...' if index < 0 || index > size - 1 
-    return if head.nil?  
-    current = @head 
-    prev = nil 
-    count = 0 
+    index = index.to_i
+    return 'Wrong index...' if index.negative? || index > size - 1
+    return if head.nil?
 
-    while count < index 
-      prev = current 
+    current = @head
+    prev = nil
+    count = 0
+
+    while count < index
+      prev = current
       count += 1
-      current = current.next_node 
+      current = current.next_node
     end
 
-    if index == 0 
+    if index.zero?
       @head = current.next_node
-      return head 
-    else 
+      head
+    else
       prev.next_node = current.next_node
-      return prev 
+      prev
     end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,9 @@
-require './node.rb'
-require './linked_list.rb'
+# frozen_string_literal: true
 
-list = LinkedList.new()
+require './node'
+require './linked_list'
 
+list = LinkedList.new
 list.app_end('two')
 list.app_end('three')
 list.prepend!('one')
@@ -10,20 +11,20 @@ list.app_end('four')
 list.app_end('five')
 
 print "\nAll nodes => "
-list.to_s 
+list.to_s
 puts "head => #{list.head_node}"
 puts "size => #{list.size}"
 puts "tail => #{list.tail}"
 puts "at_index(1) => #{list.at(1)}"
 print "remove last => #{list.pop}"
-list.to_s 
+list.to_s
 puts "contains(four) => #{list.contains?('four')}"
 puts "contains(five) => #{list.contains?('five')}"
 puts "find(one) return index => #{list.find('one')}"
 puts "find(five) return index => #{list.find('five') || 'nil'}"
 list.insert_at('insert_value', 2)
-print "Insert_at(insert_value, 2) => "
-list.to_s 
+print 'Insert_at(insert_value, 2) => '
+list.to_s
 list.remove_at(2)
-print "remove_at(2)( insert_value ) => "
+print 'remove_at(2)( insert_value ) => '
 list.to_s
