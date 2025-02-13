922fc007ccf090193612f899922b0ce66c583fc4
Ridanm <danyfox1.dm@gmail.com>
2025-02-13 17:28:55 -0300

    Correction with Rubocop

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,9 +10,9 @@ class LinkedList
 
   def append(value)
     return @head = Node.new(value) if @head.nil?
-    
+
     current = @head
-    current = current.next_node while current.next_node != nil
+    current = current.next_node until current.next_node.nil?
     current.next_node = Node.new(value)
   end
 
@@ -33,9 +33,7 @@ class LinkedList
   end
 
   def size_recursive(node = @head)
-    return 0 if node.nil?
-
-    return 1 + size_recursive(node.next_node)
+    1 + size_recursive(node.next_node)
   end
 
   def head_node
@@ -59,12 +57,13 @@ class LinkedList
   def at(index)
     index = index.to_i
     return 'Index out of range...' if index > size - 1 || index.negative?
+
     current = @head
     count = 0
     while current
       return current if count == index
 
-      current = current.next_node 
+      current = current.next_node
       count += 1
     end
   end
@@ -76,91 +75,92 @@ class LinkedList
 
     current = @head
     previous = nil
-
     while current.next_node
       previous = current
       current = current.next_node
     end
-
     previous.next_node = nil
   end
 
   def contains?(value)
     return false if head.nil?
-    current = @head
 
+    current = @head
     while current
       return true if current.data == value
+
       current = current.next_node
     end
-
     false
   end
 
-  def contains_recursive?(value, current=@head)
+  def contains_recursive?(value, current = @head)
     return false if current.nil?
     return true if current.data == value
     return false if current.next_node.nil?
+
     contains_recursive?(value, current.next_node)
   end
 
   def find(value, node = @head, counter = 0)
-    return if node.nil?
+    return nil if node.nil?
     return counter if node.data == value
+
     find(value, node.next_node, counter += 1)
   end
 
   def to_s(list = @head)
-    return puts 'nil' if list.nil? 
+    return puts 'nil' if list.nil?
+
     print "( #{list.data} ) -> "
     to_s(list.next_node)
   end
- 
+
   def insert_at(value, index)
     index = index.to_i
-    return 'Wrong index...' if index < 0 || index > size - 1
-    current = @head 
+    return if index.negative? || index > size - 1
+
+    current = @head
     insert = Node.new(value)
-    prev = nil 
+    prev = nil
     count = 0
 
-    while count < index 
-      prev = current 
-      count += 1 
-      current = current.next_node 
+    while count < index
+      prev = current
+      count += 1
+      current = current.next_node
+      insert.next_node = current
     end
 
-    if index == 0 
-      insert.next_node = current 
-      @head = insert 
-      return head
-    else 
-      insert.next_node = current
-      prev.next_node = insert 
-      return head 
+    if index.zero?
+      @head = insert
+    else
+      prev.next_node = insert
+      @head
     end
   end
 
   def remove_at(index)
-    index = index.to_i 
-    return 'Wrong index...' if index < 0 || index > size - 1 
-    return if head.nil?  
-    current = @head 
-    prev = nil 
-    count = 0 
+    index = index.to_i
+    return 'Wrong index...' if index.negative? || index > size - 1
+    return if head.nil?
 
-    while count < index 
-      prev = current 
+    current = @head
+    prev = nil
+    count = 0
+
+    while count < index
+      prev = current
       count += 1
-      current = current.next_node 
+      current = current.next_node
     end
 
-    if index == 0 
+    if index.zero?
       @head = current.next_node
-      return head 
-    else 
+      head
+    else
       prev.next_node = current.next_node
-      return prev
+      prev
     end
   end
-end 
+end
